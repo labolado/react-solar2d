a25da0e76ce75ac7e990faf5c4b297779a60d7b1
LaboLado <laboladoapp@gmail.com>
2026-03-29 15:31:54 +0800

    revert: tab items back to array child (normalizeChildren handles it)

--- navigation/TabNavigator.lua
+++ navigation/TabNavigator.lua
@@ -126,7 +126,7 @@ local function createBottomTabNavigator()
                 backgroundColor = tabBarBg,
                 width = contentW,
             },
-        }, (unpack or table.unpack)(tabItems))
+        }, tabItems)
 
         -- Layout: content area above tab bar + tab bar pinned at bottom
         local contentArea = ce("View", {
